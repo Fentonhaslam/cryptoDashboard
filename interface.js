adcdbbb691f57da764c29d07ce1d25e2ad13dfb4
fentonhaslam <fentonhaslam@gmail.com>
2019-11-05 12:23:37 +0000

    currently able to change price of chosen crypto

--- interface.js
+++ interface.js
@@ -3,17 +3,15 @@ $(document).ready(function() {
     var url = "https://api.coingecko.com/api/v3/simple/";
     var coinID = "price?ids=" + coin;
     var currency = "&vs_currencies=usd";
-    var link = url + coinID + currency;
-    console.log(link);
     $.get(url + coinID + currency, function(data) {
-      console.log(data);
-      $("#crypto1").val(data);
+      console.log(data[coin].usd);
+      $("#crypto1").text(data[coin].usd);
     });
   }
   $("#choice1").change(function() {
     var coin = $("#choice1").val();
-    console.log(coin);
-    console.log(displayCrypto1(coin));
+    //console.log(coin);
+    //console.log(displayCrypto1(coin));
     displayCrypto1(coin);
   });
 });
